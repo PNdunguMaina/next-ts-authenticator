82d06bed7377bf819fa223c9d82d4adf22588012
PNdunguMaina <ndunguwamaina@gmail.com>
2023-12-03 09:18:39 +0300

    Add client-side validation for form fields

--- components/loginForm.tsx
+++ components/loginForm.tsx
@@ -1,27 +1,55 @@
-import React, { useState } from 'react'
+import React from 'react'
+import { useFormik } from 'formik'
+import * as Yup from 'yup'
 
 const LoginForm: React.FC = () => {
-  const [username, setUsername] = useState('')
-  const [password, setPassword] = useState('')
+  // Define the validation schema using Yup
+  const validationSchema = Yup.object({
+    username: Yup.string().required('Username is required'),
+    password: Yup.string().required('Password is required'),
+  })
+
+  // Initialize formik
+  const formik = useFormik({
+    initialValues: {
+      username: '',
+      password: '',
+    },
+    validationSchema: validationSchema,
+    onSubmit: () => {
+      // handle form submission here
+      formik.resetForm()
+    },
+  })
 
   return (
-    <form>
+    <form onSubmit={formik.handleSubmit}>
       <label>
         Username:
         <input
           type="text"
-          value={username}
-          onChange={(e) => setUsername(e.target.value)}
+          name="username"
+          value={formik.values.username}
+          onBlur={formik.handleBlur}
+          onChange={formik.handleChange}
         />
+        {formik.touched.username && formik.errors.username ? (
+          <div>{formik.errors.username}</div>
+        ) : null}
       </label>
       <br />
       <label>
         Password:
         <input
           type="password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
+          name="password"
+          value={formik.values.password}
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
         />
+        {formik.touched.password && formik.errors.password ? (
+          <div>{formik.errors.password}</div>
+        ) : null}
       </label>
       <br />
       <button type="submit">Login</button>
