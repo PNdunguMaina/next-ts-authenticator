de20c290b5c37c40a18bdcfeaace0d8f553cd518
PNdunguMaina <ndunguwamaina@gmail.com>
2023-12-03 04:02:04 +0300

    Add configuration files for linters (ESLint, TypeScript) to enhance code quality and maintainability

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  extends: [
+    'next/core-web-vitals',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+  ],
+  plugins: ['@typescript-eslint'],
+  rules: {
+    // Your ESLint rules for TypeScript
+  },
+};
