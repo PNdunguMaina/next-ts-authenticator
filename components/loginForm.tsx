629adfc0af02408b5284892465db8ff967e81996
PNdunguMaina <ndunguwamaina@gmail.com>
2023-12-03 04:47:58 +0300

    Implement Typescript form component with username and password fields

---
+++ components/loginForm.tsx
@@ -0,0 +1,32 @@
+import React, { useState } from 'react'
+
+const LoginForm: React.FC = () => {
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+
+  return (
+    <form>
+      <label>
+        Username:
+        <input
+          type="text"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+      </label>
+      <br />
+      <label>
+        Password:
+        <input
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+      </label>
+      <br />
+      <button type="submit">Login</button>
+    </form>
+  )
+}
+
+export default LoginForm
