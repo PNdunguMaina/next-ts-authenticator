5efbafc6e581ed7e078f2a8b9e50ca1ec25fada6
PNdunguMaina <ndunguwamaina@gmail.com>
2023-12-03 14:58:27 +0300

    Implement client-side validation of the OTP field

--- pages/otp.tsx
+++ pages/otp.tsx
@@ -1,17 +1,48 @@
 import React, { useState } from 'react'
 import Link from 'next/link'
+import { useRouter } from 'next/router'
+import { useFormik } from 'formik'
+import * as Yup from 'yup'
+
+const OTPSchema = Yup.object().shape({
+  otp: Yup.number()
+    .integer('Please enter a valid integer OTP.')
+    .positive('Please enter a positive OTP.')
+    .required('OTP is required'),
+})
 
 const OTPPage: React.FC = () => {
+  const router = useRouter()
+
+  const formik = useFormik({
+    initialValues: {
+      otp: '',
+    },
+    validationSchema: OTPSchema,
+    onSubmit: (values) => {
+      // Handle OTP submission here
+
+      formik.resetForm()
+    },
+  })
   return (
     <div>
       <h1>Enter OTP here</h1>
-      <form>
+      <form onSubmit={formik.handleSubmit}>
         <label>
           OTP:
-          <input type="number" name="otp" value="" />
+          <input
+            type="number"
+            name="otp"
+            value={formik.values.otp}
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            min="0"
+            step="1"
+          />
         </label>
         <br />
-        <button type="submit">Submit OTP</button>
+        <button type="submit" disabled={!formik.isValid}>Submit OTP</button>
       </form>
       <p>
         <Link href="/login">Back to Login</Link>
